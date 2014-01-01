f7727ec9448c7a7fd59624dcffa2d04de7428263
lyongfly <lyongfly@live.com>
2014-01-01 21:10:40 +0800

    first version

---
+++ OpenGLDemo/src/com/lyong/opengl/renderer/AbstractRenderer.java
@@ -0,0 +1,38 @@
+package com.lyong.opengl.renderer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public abstract class AbstractRenderer implements Renderer {
+
+    public float ratio;
+    public float xrotate;
+    public float yrotate;
+    
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+      //设置清屏色
+        gl.glClearColor(0f, 0f, 0f, 1f);
+        //启用顶点缓冲区数组
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        //设置视口
+        gl.glViewport(0, 0, width, height);
+        ratio = (float)width /(float) height;
+        //投影矩阵
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        //加载单位矩阵
+        gl.glLoadIdentity();
+        //设置平截头体
+        gl.glFrustumf(-ratio, ratio, -1f, 1f, 3f, 7f);
+    }
+
+    @Override
+    public abstract void onDrawFrame(GL10 gl);
+
+}
